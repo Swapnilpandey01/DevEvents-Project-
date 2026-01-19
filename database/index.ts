f2998da94ddb2dc0c971e0f4ec95b0c05061bcd8
Swapnil Pandey <swapnilpandey0505@gmail.com>
2026-01-19 11:44:09 +0530

    Refactor exports in database index to use default imports for Event and Booking models

--- database/index.ts
+++ database/index.ts
@@ -1,2 +1,4 @@
-export { Event } from './event.model';
-export { Booking } from './booking.model';
+import Event from './event.model';
+import Booking from './booking.model';
+
+export { Event, Booking };
